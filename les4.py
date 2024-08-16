c9eb511a460965b6d4cb6bc51292d1ac0249182d
Yegor08 <yegor-tyapugin@yandex.ru>
2024-08-16 14:10:09 +0300

    Add files via upload

---
+++ les4.py
@@ -0,0 +1,5 @@
+home_work = 12
+spend_time = 1.5
+name = "Pyhton"
+mid_time = spend_time/home_work
+print("Курс:", name,","" всего задач:", home_work,","" затрачено часов:", spend_time, ","" среднее время выполнения ",mid_time, " часа.", sep="")
