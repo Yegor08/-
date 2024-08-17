0ea086f070d42e0f145625194933c2f6995f90ff
Yegor08 <yegor-tyapugin@yandex.ru>
2024-08-17 13:53:42 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = tuple([1,2,3,'a','b','c'])
+print(immutable_var)
+#tuple[0] = 22
+print(immutable_var)
+mutable_list = [1,2,3,"c","m","o","pen"]
+print(mutable_list)
+mutable_list[-1] = "pencil"
+print(mutable_list)
