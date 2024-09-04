7f1e9d47dc80f595b3d2b15aa76cb33be6a0be1d
Yegor08 <yegor-tyapugin@yandex.ru>
2024-09-04 17:39:06 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,33 +1,29 @@
+for k in range(3, 21):
+    string = ''
+    for i in range(1, k):
+        for j in range(i + 1, k):
+            if k % (i + j) == 0:
+                string += str(i) + str(j)
+    print(k, '-', string)
+
+
+
 import random
-list_1 = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-list_3 = []
-n1 = random.choice(list_1)
-#a = list_2(i) + list_1(j)
-#print(a)
-#print(n1
-for i in list_1:
-    print(i)
-    j = 0
-    while j < i-2:
-        k = j+1
-
-        while k < i-1:
-            a = list_2[j]+list_2[j+k]
-            if i % a == 0:
-
-                print(list_2[j])
-                print(list_2[j+k])
-            k = k+1
-        j += 1
+list_1 = range(3, 21)
+k = random.choice(list_1)
+string = ''
+for i in range(1, k):
+    for j in range(i + 1, k):
+        if k % (i + j) == 0:
+            string += str(i) + str(j)
+print(k, '-', string)
 
 
-    #print()
+k = int(input('Введите число - '))
+string = ''
+for i in range(1, k):
+    for j in range(i + 1, k):
+        if k % (i + j) == 0:
+            string += str(i) + str(j)
+print(k, '-', string)
 
-
-#lakky_nom = int(input('Введите число - '))
-#for i in list_2:
-    #if lakky_nom % 2 == 0:
-        #list_3.append(i)
-        #break
-#print(list_3)
